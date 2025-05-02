790aaaa382ac3e3aa63b9fdf1117fd121316d88a
Ilya Sheprut <optozorax@gmail.com>
2025-05-02 22:38:26 +0300

    SCENES[companion_cube_texture]: add companion cube texture scene

--- src/gui/scenes.rs
+++ src/gui/scenes.rs
@@ -56,6 +56,12 @@ impl Default for Scenes {
                         hidden: true,
                     },
                     Scene {
+                        name: "Companion cube texture",
+                        link: "companion_cube_texture",
+                        content: include_str!("../../scenes/companion_cube_texture.ron"),
+                        hidden: true,
+                    },
+                    Scene {
                         name: "Teleportation degrees (for video)",
                         link: "teleportation_degrees",
                         content: include_str!("../../scenes/teleportation_degrees.ron"),
