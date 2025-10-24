b2f3ca97939a4f86f278597bb25aaaca90b1dc8d
Ilya Sheprut <optozorax@gmail.com>
2025-10-24 04:56:19 +0300

    add portal_lab secret demo I believe no one is gonna look here

---
+++ portal_lab/sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'portal-lab-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './portal_lab.js',
+  './portal_lab_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
